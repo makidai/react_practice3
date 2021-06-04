998bef67019a8c39fcabd6989b04c055ec218dd8
Ryodai Makino <davinci0327@icloud.com>
2021-06-04 09:55:03 +0900

    props to context

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
     if (action.type === "USER_INPUT") {
@@ -41,22 +42,21 @@ const Login = (props) => {
         isValid: null,
     });
 
-    const { isValid: emailIsValid } = emailState;
-    const { isValid: passwordIsValid} = passwordState;
+    const authCtx = useContext(AuthContext);
 
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
 
     useEffect(() => {
-      const identifier = setTimeout(() => {
-        console.log('test');
-        setFormIsValid(
-          emailIsValid && passwordIsValid
-        );
-      }, 500);
+        const identifier = setTimeout(() => {
+            console.log("test");
+            setFormIsValid(emailIsValid && passwordIsValid);
+        }, 500);
 
-      return () => {
-        console.log('clean');
-        clearTimeout(identifier);
-      };
+        return () => {
+            console.log("clean");
+            clearTimeout(identifier);
+        };
     }, [emailIsValid, passwordIsValid]);
 
     const emailChangeHandler = (event) => {
@@ -68,7 +68,7 @@ const Login = (props) => {
     };
 
     const passwordChangeHandler = (event) => {
-        dispatchPassword({type: 'USER_INPUT', val: event.target.value})
+        dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
         // setFormIsValid(
         //     emailState.isValid && event.target.value.trim().length > 6
@@ -80,12 +80,12 @@ const Login = (props) => {
     };
 
     const validatePasswordHandler = () => {
-        dispatchPassword({type: 'INPUT_BLUR'})
+        dispatchPassword({ type: "INPUT_BLUR" });
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
     return (
